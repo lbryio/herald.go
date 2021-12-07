2ee8c62d131fdc558dd967f08ca11691376734b9
Jeffrey Picard <jeff@jeffreypicard.com>
2021-12-06 22:59:05 -0500

    asdf

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export CGO_LDFLAGS="-L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lsnappy -llz4 -lzstd"
+export CGO_CFLAGS="-I/usr/local/include/rocksdb"
 go build .
 go test -v -race ./...
