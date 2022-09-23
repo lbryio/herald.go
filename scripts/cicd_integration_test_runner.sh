76e639b757f1a914be802c7df9c660d51a289903
Jeffrey Picard <jeff@jeffreypicard.com>
2022-09-23 13:51:25 +0000

    update

--- scripts/cicd_integration_test_runner.sh
+++ scripts/cicd_integration_test_runner.sh
@@ -5,5 +5,10 @@
 # simple script to kick off herald and call the integration testing
 # script
 #
+# N.B. this currently just works locally until we figure a way to have
+# the data in the cicd environment.
+#
 
+./herald serve --db-path /mnt/sdb1/wallet_server/_data/lbry-rocksdb &
 
+./integration_tests.sh
